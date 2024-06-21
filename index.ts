e69db1fce4220199fe3c716082fd937fec7d2ee7
Sejal Jagtap <sejaljagtap1309@gmail.com>
2024-06-22 01:44:31 +0530

    backend socket.io

---
+++ index.ts
@@ -0,0 +1,39 @@
+// declare var require: any
+const express = require('express');
+const http = require('http')
+
+const app = express()
+
+const server = http.createServer(app)
+
+import { Server } from 'socket.io'
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+})
+
+type Point = {
+    x: number, y: number
+}
+
+type DrawLine = {
+    prevPoint: Point | null
+    currentPoint: Point
+    color: string
+}
+
+io.on('connection', (socket) => {
+    console.log("connected")
+    socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLine) => {
+        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
+    })
+    socket.on('clear', () => {
+        io.emit('clear')
+    })
+})
+
+server.listen(3001, () => {
+    console.log("server listening on port 3001")
+})

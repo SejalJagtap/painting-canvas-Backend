b3762e74f1a2c11014c55bbf7c68bfb02eb88eda
Sejal Jagtap <sejaljagtap1309@gmail.com>
2024-06-26 02:22:06 +0530

    participant list socket created

--- index.ts
+++ index.ts
@@ -1,39 +1,51 @@
 const express = require('express');
-const http = require('http')
+const http = require('http');
 
-const app = express()
-
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
 const { Server, Socket } = require('socket.io');
-
 
 const io = new Server(server, {
     cors: {
         origin: '*',
     }
-})
+});
 
 type Point = {
     x: number, y: number
-}
+};
 
 type DrawLine = {
-    prevPoint: Point | null
-    currentPoint: Point
+    prevPoint: Point | null,
+    currentPoint: Point,
     color: string
-}
+};
+
+let participants: { id: string, name: string }[] = [];
 
 io.on('connection', (socket: typeof Socket) => {
-    console.log("connected")
+    console.log("connected");
+
     socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLine) => {
-        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
-    })
+        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color });
+    });
+
     socket.on('clear', () => {
-        io.emit('clear')
-    })
-})
+        io.emit('clear');
+    });
+
+    socket.on('name', (name: string) => {
+        participants.push({ id: socket.id, name });
+        io.emit('participants', participants.map(participant => participant.name));
+    });
+
+    socket.on('disconnect', () => {
+        participants = participants.filter(participant => participant.id !== socket.id);
+        io.emit('participants', participants.map(participant => participant.name));
+    });
+});
 
 server.listen(3001, () => {
-    console.log("server listening on port 3001")
-})
+    console.log("server listening on port 3001");
+});
